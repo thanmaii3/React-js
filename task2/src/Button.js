c8d08a1c9c25ac8fd32d1fb2c38f6d3516a41024
Thanmai Veeralanka <20b01a12i3@svecw.edu.in>
2022-02-11 15:38:09 +0530

    assignment4

---
+++ task2/src/Button.js
@@ -0,0 +1,18 @@
+function Button(){
+    
+    
+        return(
+          <div class="ui cards">
+  <div class="card">
+
+      <div className="extra content">
+      <div class="ui two buttons">
+        <div class="ui basic green button">Accept</div>
+        <div class="ui basic red button">Decline</div>
+      </div>
+    </div>
+    </div>
+    </div>
+    )
+}
+export default Button;
--- task2/src/index.js
+++ task2/src/index.js
@@ -1,88 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Faker from 'faker';
+import CommentBox from './CommentBox.js';
+import Button from './Button.js';
 
-function Comment(){
 
-    return(
-        <table className="ui very basic collapsing celled table">
-        <thead>
-          <tr><th>Employee</th>
-          <th>Correct Guesses</th>
-        </tr></thead>
-        <tbody>
-          <tr>
-            <td>
-              <h4 className="ui image header">
-                <img src={Faker.image.image()}/>
-                <div className="content">
-                  Lena
-                  <div className="sub header">Human Resources
-                </div>
-              </div>
-            </h4></td>
-            <td>
-              22
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h4 className="ui image header">
-                <img src={Faker.image.image()}/>
-                <div className="content">
-                  Matthew
-                  <div className="sub header">Fabric Design
-                </div>
-              </div>
-            </h4></td>
-            <td>
-              15
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h4 className="ui image header">
-                <img src={Faker.image.image()}/>
-                <div className="content">
-                  Lindsay
-                  <div className="sub header">Entertainment
-                </div>
-              </div>
-            </h4></td>
-            <td>
-              12
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h4 className="ui image header">
-                <img src={Faker.image.image()}/>
-                <div className="content">
-                  Mark
-                  <div className="sub header">Executive
-                </div>
-              </div>
-            </h4></td>
-            <td>
-              11
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      
-    )
-}
+const App = () => {
+  return (
+    <>
+  
+  <CommentBox />
+  <Button />
+  
+  </>
+
+  )
+  
+};
 
 
 
 
 ReactDOM.render(
-    <>
-    <Comment/>
-    
-     
-
-    </>,
+    <App />,
     document.getElementById("root")
 
 );
